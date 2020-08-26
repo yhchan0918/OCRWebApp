53289f245319f86e1c8bc1d66187fed385dc419c
Chan Yun Hong <yhchan0918@gmail.com>
2020-08-26 17:15:48 +0800

    delete unwanted file and basic ui

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+<nav>
+  <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h1>CopyCat</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      
+</v-app-bar>
+</nav>
+
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <v-form v-model="valid">
+      <v-container>
+        <v-row justify="space-between">
+          <v-col cols="12" class="text-center">
+            <v-file-input multiple label="File input"></v-file-input>
+          </v-col>
+        </v-row>
+        <v-row justify="space-between">
+          <v-col cols="6" class="text-center"> </v-col>
+          <v-col cols="6" class="text-right">
+            <v-btn class="mr-4">Start OCR</v-btn>
+            <v-btn>Clear</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-container>
+      <v-row justify="space-between">
+        <v-col cols="12" class="text-center">
+          <v-textarea
+            auto-grow
+            filled
+            color="deep-purple"
+            label="Text"
+            rows="1"
+            v-model="result"
+          >
+          </v-textarea>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      file: "",
+      result: "im a bitch",
+    };
+  },
+  components: {},
+};
+</script>
